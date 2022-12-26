c000fa09ca4232a2df49ca4c054e1340f787fd58
Alex <32147165+alexanderjulianmartinez@users.noreply.github.com>
2022-12-26 21:00:52 +0000

    Updating db_conn file with create table schemas

--- option_trading_alerts/db_conn.py
+++ option_trading_alerts/db_conn.py
@@ -14,6 +14,20 @@ def create_conn(db_file):
     return conn
 
 
+def create_table(conn, create_sql_stmt):
+    """ create a table from the create_table_sql statement
+        :param conn: Connection object
+        :param create_sql_stmt: a CREATE TABLE statement
+        :return:
+        """
+    try:
+        c = conn.cursor()
+        c.execute(create_sql_stmt)
+        print('Table successfully created!')
+    except Error as e:
+        print(e)
+
+
 def add_option_alert(conn, option):
     """
     Create a new option alert into the options_alerts table
@@ -21,7 +35,7 @@ def add_option_alert(conn, option):
     :param option:
     :return: option id
     """
-    sql = ''' INSERT INTO options_alerts(ticker, option_type, alert_expire_date, alert_premium, curr_price)
+    sql = ''' INSERT INTO options_alerts(ticker, option_type, expire_date, strike_price, trade_premium)
               VALUES(?, ?, ?, ?, ?) '''
     cur = conn.cursor()
     cur.execute(sql, option)
@@ -37,8 +51,8 @@ def create_trade(conn, trade):
     :return: trade id
     """
     sql = ''' 
-        INSERT INTO trades(option_id, ticker, option_type, expire_date, strike_price, trade_premium)
-        VALUES(?, ?, ?, ?, ?, ?)
+        INSERT INTO trades(option_id, ticker, option_type, expire_date, strike_price, trade_premium, curr_premium)
+        VALUES(?, ?, ?, ?, ?, ?, ?)
     '''
     cur = conn.cursor()
     cur.execute(sql, trade)
@@ -46,26 +60,64 @@ def create_trade(conn, trade):
     return cur.lastrowid
 
 
-def main():
+def main(ticker, option_type, expire_date, strike_price, alert_premium, curr_premium):
     database = r"C:\sqlite\db\pythonsqlite.db"
+
+    sql_create_options_alerts_table = """ CREATE TABLE IF NOT EXISTS options_alerts (
+                                              id integer PRIMARY KEY AUTOINCREMENT,
+                                              ticker text NOT NULL,
+                                              option_type text,
+                                              expire_date text,
+                                              strike_price text,
+                                              alert_premium text,
+                                              trade_premium text
+                                          ); """
+
+    sql_create_trades_table = """CREATE TABLE IF NOT EXISTS trades (
+                                      id integer PRIMARY KEY AUTOINCREMENT,
+                                      option_id integer NOT NULL,
+                                      ticker text NOT NULL,
+                                      option_type text,
+                                      expire_date text,
+                                      strike_price text,
+                                      trade_premium text,
+                                      curr_premium text
+                                        
+                                 );"""
 
     # create a database connection
     conn = create_conn(database)
     with conn:
-        ticker = 'AAPL'
-        option_type = 'CALL'
-        expire_date = '12-30-2022'
-        strike_price = '132.0'
-        alert_premium = '1.98'
-        curr_premium = alert_premium
 
-        option = (ticker, option_type, expire_date, strike_price, alert_premium, curr_premium)
+        print('Creating options_alerts table...')
+        create_table(conn, sql_create_options_alerts_table)
+
+        print('Creating trades table...')
+        create_table(conn, sql_create_trades_table)
+
+        option = (ticker, option_type, expire_date, strike_price, curr_premium)
         option_id = add_option_alert(conn, option)
+        print(f'Stored Option Alert #{option_id} in option_alerts table')
 
         trade = (option_id, ticker, option_type, expire_date, strike_price, alert_premium, curr_premium)
 
-        create_trade(conn, trade)
+        trade_id = create_trade(conn, trade)
+        print(f'Stored Trade #{trade_id} in trade table')
 
 
 if __name__ == '__main__':
-    main()
+    example_ticker = 'AAPL'
+    example_option_type = 'CALL'
+    example_expire_date = '12-30-2022'
+    example_strike_price = '132.0'
+    example_alert_premium = '1.98'
+    example_curr_premium = example_alert_premium
+    main(
+        example_ticker,
+        example_option_type,
+        example_expire_date,
+        example_strike_price,
+        example_alert_premium,
+        example_curr_premium
+    )
+
